f6977871c2354f88dce9cb02cbd833ad7d37bdec
inosh611 <inoshharsha531@gmail.com>
2026-03-18 10:15:11 +0530

    Project setup

---
+++ myFunctions.js
@@ -0,0 +1,13 @@
+const math = (number1, number2, number3, number4)=>{
+    let a = number1+number2*2;
+    if(number3){
+        a += number3 - 5;
+    }
+    else if(number4){
+            a *= number4; 
+    }
+
+    return a;
+}
+
+console.log(math(2,3));
